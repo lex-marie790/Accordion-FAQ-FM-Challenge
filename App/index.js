d706d9778d753cc635135179cc045759d5914e18
lex-marie790 <lexi.marie790@gmail.com>
2021-02-14 21:54:04 -0800

    getting the general idea of the layout, no where close

---
+++ App/index.js
@@ -0,0 +1,9 @@
+// function show() {
+//     var image = document.getElementById('arrow');
+//     var p = document.getElementByClass('answer')
+//     if (image.style.display === 'none') {
+//         p.style.display = 'block';
+//     } else {
+//         p.style.display = 'none'
+//     }
+// }
